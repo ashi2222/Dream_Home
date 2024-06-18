ad5d6fab63748c0dcb1805c8a65a3b1033846a58
Ashish Patel <96334594+ashi2222@users.noreply.github.com>
2024-06-18 17:16:40 +0530

    updated-myapp-url

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ import cookieParser from 'cookie-parser';
 import cors from "cors"
 const app = express();
 
-app.use(cors({origin: "http://localhost:3000" , credentials: true}));
+app.use(cors({origin: "https://dream-home-myapp.onrender.com" , credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
